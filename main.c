90a2cf034038a3ae68a6d0bedf2dcccb6f71229c
Dmitry Egorov <mail@degorov.com>
2012-09-17 23:28:48 +0400

    popen - pages count

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-	printf("TYeBook Base\n");
+
+int main(int argc, char const *argv[]) {
+
+	FILE *fp;
+	char line[130];
+
+	char *found;
+
+	int PAGES = 0;
+   
+	fp = popen("wutils\\pdfinfo.exe temp\\simpler.pdf", "r");
+
+	while (fgets(line, sizeof line, fp)) {
+		// printf("%s", line);
+
+		if (found = strstr(line, "Pages:")) {
+			PAGES = atoi(found+16);
+			printf("PAGES = %d\n", PAGES);
+		}
+
+	}
+	pclose(fp);
+	
 	return 0;
+
 }
