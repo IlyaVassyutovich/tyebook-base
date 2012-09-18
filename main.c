f508f65f348da534903cf864088068f6f552b9af
Dmitry Egorov <mail@degorov.com>
2012-09-18 16:30:31 +0400

    Overlapping done

--- main.c
+++ main.c
@@ -14,8 +14,8 @@ int main(int argc, char const *argv[]) {
 
 	int PAGE, PAGES = 0;
 
-	char STAGE1[1025];
-	char STAGE2[1025];
+	char STAGE1[2048];
+	char STAGE2[2048];
 
 	int tmp = 0;
 
@@ -34,13 +34,9 @@ int main(int argc, char const *argv[]) {
 
  	WIDTH = atoi(argv[2]);
  	HEIGHT = atoi(argv[3]);
- 	OVERLAP = atoi(argv[4]);
+ 	OVERLAP = atoi(argv[4])*WIDTH;
  	FRAME = WIDTH*HEIGHT;
 
- 	if (WIDTH+1 > sizeof line) {
- 		printf("ERROR: Maximum width supported is 1024px\n");
- 		return 0;
- 	}
 
 	fp = popen("wutils\\pdfinfo.exe temp\\simpler.pdf", "r");
 
@@ -58,14 +54,10 @@ int main(int argc, char const *argv[]) {
 	BUFSIZE = WIDTH;
 	BUFFER = malloc(BUFSIZE);
 	START = BUFFER;
-	
 
-	for (PAGE=1; PAGE<=20; PAGE++) {
+	for (PAGE=2; PAGE<=4; PAGE++) {
 
 		printf("PAGE: %04d\n", PAGE);
-
-//		tmp = sprintf(STAGE1, "wutils\\pdftoppm -gray -r 300 -f %d -l %d \"%s\" | wutils\\convert - -fuzz 1%% -trim +repage -resize 800 -bordercolor white -border 0x10 -bordercolor black -border 0x5 -type GrayScale -depth 8 gray:\"temp\\temp%04d.gray\"", PAGE, PAGE, argv[1], PAGE);
-//		tmp = sprintf(STAGE2, "wutils\\convert -size %sx%s -depth 8 \"temp%04d.gray\" -rotate -90 +repage -strip -type GrayScale -depth 4 -compress Zip -quality 100 \"temp\\temp%04d.pdf\"", argv[2], argv[3], PAGE, PAGE);
 
 		tmp = sprintf(STAGE1, "wutils\\pdftoppm -gray -r 300 -f %d -l %d \"%s\" | wutils\\convert - -fuzz 1%% -trim +repage -resize 800 -bordercolor white -border 0x10 -bordercolor black -border 0x5 -type GrayScale -depth 8 gray:-", PAGE, PAGE, argv[1]);
 
@@ -73,15 +65,22 @@ int main(int argc, char const *argv[]) {
 
 		while ((tmp = fread(START, WIDTH, 1, fp)) > 0) {
 
-			if (BUFSIZE % (FRAME) == 0) {
-				// got full page - reset buffer and parse
-				BUFSIZE=0;
+			if (BUFSIZE % FRAME == 0) {
 
 				// for debug
 				tmp = sprintf(STAGE2, "temp\\tezt%04d.raw", lcntr++);
 				fp2 = fopen(STAGE2, "wb");
 				fwrite(BUFFER, FRAME, 1, fp2);		
 				fclose(fp2);
+				// end debug
+
+				// tmp = sprintf(STAGE2, "wutils\\convert -size %sx%s -depth 8 \"temp%04d.gray\" -rotate -90 +repage -strip -type GrayScale -depth 4 -compress Zip -quality 100 \"temp\\temp%04d.pdf\"", argv[2], argv[3], PAGE, PAGE);				
+
+				// copy overlapping data from buffer end
+				memmove(BUFFER, BUFFER+FRAME-OVERLAP, OVERLAP-WIDTH);
+				// black line
+				memset(BUFFER+OVERLAP-WIDTH, 0, WIDTH);
+				BUFSIZE=OVERLAP;				
 
 			}
 			BUFFER = realloc(BUFFER, BUFSIZE+=WIDTH);
@@ -92,14 +91,14 @@ int main(int argc, char const *argv[]) {
 
 	}
 
+	// delete last buffer line (garbage)
+	BUFFER = realloc(BUFFER, BUFSIZE-=WIDTH);
 
 	// for debug
-
-	BUFFER = realloc(BUFFER, BUFSIZE-=WIDTH);
 	fp2 = fopen("temp\\teztLLLL.raw", "wb");
 	fwrite(BUFFER, BUFSIZE, 1, fp2);		
 	fclose(fp2);
-//	tmp = getchar();
+	// end debug
 	
 	return 0;
 
